cd0159a9185007237d209ea5281303d4b7c62a00
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-04-11 21:33:05 +0530

    Updates

--- assets/Portfolio-CoJy0xPG.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-C9rY8j2E.js";import{I as o,P as r}from"./Profile-BC9ZfooH.js";function t(){return s.jsx("div",{className:"mt-10 mb-16 flex justify-center",children:s.jsx("img",{src:"./assets/images/commingSoon.gif",alt:"image"})})}function n(){return s.jsxs(s.Fragment,{children:[s.jsx(o,{}),s.jsx(r,{heading:"Portfolio"}),s.jsx(t,{})]})}export{n as default};
---
+++ assets/Portfolio-IjnRzZ2F.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CadlPTFe.js";import{I as o,P as r}from"./Profile-D19uX8Kq.js";function t(){return s.jsx("div",{className:"mt-10 mb-16 flex justify-center",children:s.jsx("img",{src:"./assets/images/commingSoon.gif",alt:"image"})})}function n(){return s.jsxs(s.Fragment,{children:[s.jsx(o,{}),s.jsx(r,{heading:"Portfolio"}),s.jsx(t,{})]})}export{n as default};
