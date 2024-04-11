866300dd640e98904f5917fdffe301c5f9f82eea
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-04-11 22:06:02 +0530

    path correction of comming soon image

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 function Works() {
   return (
     <div className="mt-10 mb-16 flex justify-center">
-      <img src="./assets/images/commingSoon.gif" alt="image" />
+      <img src="profile/assets/images/commingSoon.gif" alt="image" />
     </div>
   );
 }
