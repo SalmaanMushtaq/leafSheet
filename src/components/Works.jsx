4eeac1d39eb5bdcda3f0c15aebc91796e24a167f
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-04-11 21:23:25 +0530

    comming soon image added cv corrction

---
+++ src/components/Works.jsx
@@ -0,0 +1,9 @@
+function Works() {
+  return (
+    <div className="mt-10 mb-16 flex justify-center">
+      <img src="./assets/images/commingSoon.gif" alt="image" />
+    </div>
+  );
+}
+
+export default Works;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,17 @@
 import Introduction from "../components/Introduction";
-import Options from "../components/Options";
+// import Options from "../components/Options";
 import Profile from "../components/Profile";
+import Works from "../components/Works";
 
 function Portfolio() {
   return (
     <>
       <Introduction />
       <Profile heading="Portfolio" />
-      <Options options={["Education", "Technical Skills", "Experience"]} />
-      <Options options={["Education", "Technical Skills", "Experience"]} />
-      <Options options={["Education", "Technical Skills", "Experience"]} />
+      {/* <Options options={["Education", "Technical Skills", "Experience"]} /> */}
+      {/* <Options options={["Education", "Technical Skills", "Experience"]} /> */}
+      {/* <Options options={["Education", "Technical Skills", "Experience"]} /> */}
+      <Works />
     </>
   );
 }
