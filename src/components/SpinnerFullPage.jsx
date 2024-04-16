1cc655548a4227adddbad70aebd49fcdaa65c54c
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-04-16 22:42:12 +0530

    spinner changed to the center of the screen

--- src/components/SpinnerFullPage.jsx
+++ src/components/SpinnerFullPage.jsx
@@ -1,23 +1,21 @@
 import styles from "./SpinnerFullPage.module.css";
 const SpinnerFullPage = () => {
   return (
-    <>
-      <div className={styles.pl}>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__dot}></div>
-        <div className={styles.pl__text}>Loading…</div>
-      </div>
-    </>
+    <div className={styles.pl}>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__dot}></div>
+      <div className={styles.pl__text}>Loading…</div>
+    </div>
   );
 };
 
