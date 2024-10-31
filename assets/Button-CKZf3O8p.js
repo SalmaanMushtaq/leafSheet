1ec4a6959f96ebfa56bb56ed8b7b709214a63207
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-10-31 20:18:50 +0530

    Updates

--- assets/Button-CKZf3O8p.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-LS8U_KAV.js";function r({text:t}){return n.jsx("button",{className:"p-1 px-6 bg-btnBackground rounded-full",children:t})}export{r as B};
---
+++ assets/Button-DVeS95lF.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-Brzi-mtp.js";function r({text:t}){return n.jsx("button",{className:"p-1 px-6 bg-btnBackground rounded-full",children:t})}export{r as B};
